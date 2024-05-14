382e1313fee05715c932cd6783ec8aa0bcd923ad
Darlington Omoifo <darlington.omoifo@solibri.com>
2024-05-14 11:55:28 -0400

    Use remote name for package

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthGuard } from './auth.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: '60s' },
     }),
   ],
   providers: [
